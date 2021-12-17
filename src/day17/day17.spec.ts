85dbf8d0db27b0b8ac567d575669b88960b8c7fc
Daniel Morvai <daniel.morvai@emarsys.com>
2021-12-17 13:33:55 +0100

    Day 17: trick shot, part 2 - remove only

--- src/day17/day17.spec.ts
+++ src/day17/day17.spec.ts
@@ -1,7 +1,7 @@
 import {Destination, findHighestValue, findPossibleVelocities} from "./day17";
 import {expect} from "chai";
 
-describe.only('Day 15 tests', () => {
+describe('Day 15 tests', () => {
   const testDestination: Destination = {minX: 20, maxX: 30, minY: -10, maxY: -5};
 
   it('should find highest value for test destination', () => {
